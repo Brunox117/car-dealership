67858a70dc633777ea73203a7f4e12d0f79acd1c
Bruno Jimenez <omarjm0910@gmail.com>
2025-06-29 19:38:26 -0600

    Refactor CarsController to use CarsService for car data retrieval and implement CarsService with methods to find all cars and find a car by ID.

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CarsService {}
+export class CarsService {
+  private cars = [
+    {
+      id: 1,
+      brand: 'Toyota',
+      model: 'Yaris S',
+    },
+    {
+      id: 2,
+      brand: 'Honda',
+      model: 'Honda Fit',
+    },
+    {
+      id: 3,
+      brand: 'Tesla',
+      model: 'Model S',
+    },
+  ];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOneById(id: number) {
+    const car = this.cars.find((car) => car.id === id);
+    return car;
+  }
+}
